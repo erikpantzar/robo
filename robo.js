4bbfed1be5d7cf279179255950bbc7b2ffb477e7
epantzar <erxz0r@gmail.com>
2016-09-20 02:33:05 +0200

    robot movemnt

--- robo.js
+++ robo.js
@@ -1,5 +1,3 @@
-
-
 // 5x5 room
 // point
 // getStartPos
@@ -7,3 +5,74 @@
 // L R F
 // unit test
 
+
+const dir = ['N', 'E', 'S', 'W'];
+const l = dir.length;
+
+function changeDirection(input='R', robot=walle) {
+  let direction = robot.getPosition().direction, bool = true;
+  if (input === 'V' || input === 'L') {
+    bool = false;
+  }  // else input === 'H' || input === 'R'
+
+  if(bool) {
+    // array to one right
+    dir.push(dir.shift());
+  } else {
+    dir.unshift(dir[3]);
+    dir.pop();
+  }
+  
+  console.log(input, robot, dir[0]);
+  return dir[0];
+}
+
+class Robot {
+  constructor(x, y, direction='N') {
+    this.x = x;
+    this.y = y;
+    this.direction = direction;
+  }
+
+  getPosition() {
+    return {
+      point: { x: this.x, y: this.y },
+      direction: this.direction
+    }
+  }
+
+  action(move="VHG") {
+    move = move.split('');
+    move.forEach((m)=> {
+      if (m === 'G' || m === 'F') {
+        this.forward(this.direction);
+      } else {
+        this.direction = changeDirection(m, this);
+      }
+    })
+    
+  }
+
+  forward(direction='N') {
+    switch(direction) {
+      case 'N':
+      this.y++
+      break;
+      case 'E':
+      this.x++
+      break;
+      case 'S':
+      this.y--
+      break;
+      default:
+      this.x--
+    }
+
+    console.log(this);
+  }
+}
+
+
+let walle = new Robot(1,2);
+//console.log(walle.getPosition());
+walle.action('RLG');
